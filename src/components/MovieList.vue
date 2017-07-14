63065e1b9494827dff0b85542362ad7a80914477
amarangonihengling <ahengling@azi.com.br>
2017-07-14 01:15:56 -0400

    Creating basic components for displaying the movies and genre filters.

---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="movie-list">
+        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+    </div>
+</template>
+<script>
+    import genres from '../util/genres';
+
+    export default {
+        props: [ 'genre', 'time' ],
+        data() {
+            return {
+                movies: [
+                    { title: 'Kill Bill', genre: genres.CRIME },
+                    { title: 'Home Alone', genre: genres.COMEDY },
+                    { title: 'Austin Powers 2', genre: genres.COMEDY }
+                ]
+            }
+        },
+        methods: {
+            moviePassesGenreFilter(movie) {
+                if (!this.genre.length) {
+                    return true;
+                } else {
+                    return this.genre.find(genre => movie.genre === genre);
+                }
+            }
+        },
+        computed: {
+            filteredMovies() {
+                return this.movies.filter(this.moviePassesGenreFilter);
+            }
+        }
+    }
+</script>
